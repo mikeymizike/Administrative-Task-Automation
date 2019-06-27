ec34e3e845018adc991660ba14d165e2ed1a993e
mikeymizike <45925345+mikeymizike@users.noreply.github.com>
2019-06-27 10:04:56 -0400

    Update vCard creator.py
    Updated to correct from improper Zip code management

--- vCard Generator/vCard creator.py
+++ vCard Generator/vCard creator.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-filepath = "C:\\Desktop\\vCard Generator\\"
-excelfile = "C:\\vCard Generator\\vCard Data.xlsx"
-df = pd.read_excel(excelfile, usecols="A:J").fillna('')
+filepath = "C:\\Users\\braaten\\Desktop\\vCards\\"
+excelfile = "C:\\Users\\braaten\\OneDrive - NCQA\\scratch worksheet\\scratch worksheet.xlsx"
+df = pd.read_excel(excelfile, sheet_name = "vCards", usecols="A:J", dtype={"zipcode":'str'}).dropna()
 last = df["Last Name"].tolist()
 first = df["First Name"].tolist()
 org = df["Organization"].tolist()
@@ -12,8 +12,10 @@ email = df["email"].tolist()
 address = df["address"].tolist()
 city = df["City"].tolist()
 state = df["State"].tolist()
-zipcode = df["Zip"].tolist()
+df['zipcode'] = df['zipcode'].astype(str).str.zfill(5)
+zipcode = df["zipcode"].tolist()
 country = "United States of America"
+
 for i in range(0,len(last)):
     file = open(filepath+first[i]+" "+last[i]+'.vcf', 'w')
     file.write("BEGIN:VCARD\nVERSION:2.1\nN;LANGUAGE=en-us:\n")
@@ -22,7 +24,7 @@ for i in range(0,len(last)):
     file.write("ORG:"+org[i]+"\n")
     file.write("TITLE:"+title[i]+"\n")
     file.write("TEL;WORK;VOICE:"+"("+str(phone[i])[:3]+") "+str(phone[i])[3:6]+"-"+str(phone[i])[6:10]+"\n")
-    file.write("ADR;WORK;PREF:;;"+address[i]+";"+city[i]+";"+state[i]+";"+str(zipcode[i])+";"+country[i]+"\n")
+    file.write("ADR;WORK;PREF:;;"+address[i]+";"+city[i]+";"+state[i]+";"+str(zipcode[i])+";"+country+"\n")
     file.write("EMAIL;PREF;INTERNET:"+email[i]+"\n")
     file.write("END:VCARD")
     file.close()
